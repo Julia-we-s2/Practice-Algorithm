128135bcae037a547e801c395faeccb0f571af96
위연주 <thizzyzz@naver.com>
2022-02-24 21:43:25 +0900

    :dizzy: Solve boj 02206 벽 부수고 이동하기

---
+++ BOJ/bfs/02206-벽_부수고_이동하기.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+
+def bfs():
+    queue = deque()
+    queue.append((0, 0, 0))
+    visited[0][0][0] = 1
+
+    while queue:
+        x, y, wall = queue.popleft()
+
+        if x == n-1 and y == m-1:
+            return visited[x][y][wall]
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny][wall] == 0:
+                if arr[nx][ny] == 0:
+                    queue.append((nx, ny, wall))
+                    visited[nx][ny][wall] = visited[x][y][wall] + 1
+
+                if arr[nx][ny] == 1 and wall == 0:
+                    queue.append((nx, ny, 1))
+                    visited[nx][ny][1] = visited[x][y][wall] + 1
+                print(nx, ny)
+                for row in visited:
+                    print(*row)
+    return -1
+
+
+n, m = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(n)]
+visited = [[[0 for _ in range(2)] for _ in range(m)] for _ in range(n)]
+
+print(bfs())
+
+
+
