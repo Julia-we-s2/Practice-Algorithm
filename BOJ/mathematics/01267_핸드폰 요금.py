b9dd768348a588e85344c120431cf444083d354a
위연주 <thizzyzz@naver.com>
2022-02-26 09:57:41 +0900

    :dizzy: Solve boj 01267 핸드폰 요금

---
+++ BOJ/mathematics/01267_핸드폰 요금.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+li = list(map(int, input().split()))
+
+y = m = 0
+
+for i in li:
+    y += (i//30 + 1) * 10
+    m += (i//60 + 1) * 15
+
+if y > m:
+  print("M", m)
+elif y < m:
+  print("Y", y)
+else:
+  print("Y M", m)
