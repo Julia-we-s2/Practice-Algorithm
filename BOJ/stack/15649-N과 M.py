86d4305fe0acfb954b0c69025b7093d07cf2e5f4
위연주 <thizzyzz@naver.com>
2022-03-01 12:11:00 +0900

    :dizzy: Solve boj 15649 N과 M

---
+++ BOJ/stack/15649-N과 M.py
@@ -0,0 +1,15 @@
+def dfs():
+    if len(li) == m:
+        print(*li)
+        return 
+
+    for i in range(1, n+1):
+        if i not in li:
+            li.append(i)
+            dfs()
+            li.pop()
+
+
+n, m = map(int, input().split())
+li = []
+dfs()
