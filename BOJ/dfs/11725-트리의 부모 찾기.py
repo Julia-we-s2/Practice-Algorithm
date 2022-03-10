680b7ee4ef66a8470bd4507939a5d0075ac6efab
위연주 <thizzyzz@naver.com>
2022-03-10 22:20:14 +0900

    :dizzy: Solve boj 11725 트리의 부모 찾기

---
+++ BOJ/dfs/11725-트리의 부모 찾기.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+
+def dfs(s):
+    for i in graph[s]:
+        if visited[i] == 0:
+            visited[i] = s
+            dfs(i)
+
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+
+for i in range(n-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+dfs(1)
+
+for i in range(2, n+1):
+    print(visited[i])
