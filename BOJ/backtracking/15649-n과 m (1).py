f31dc5c272c674633ea20f68c311203c844ac03d
위연주 <thizzyzz@naver.com>
2022-06-06 13:45:32 +0900

    :dizzy: code : Solve boj 15649 n과 m (1)

---
+++ BOJ/backtracking/15649-n과 m (1).py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+li = []
+
+
+def dfs():
+  if len(li) == m:
+    print(' '.join(map(str, li)))
+    return
+
+  for i in range(1, n + 1):
+    if i not in li:
+      li.append(i)
+      dfs()
+      li.pop()
+
+
+dfs()
