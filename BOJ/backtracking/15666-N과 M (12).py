940d6b541a9e2b7f1c5f692261644f7c6b2bdf85
위연주 <thizzyzz@naver.com>
2022-06-11 21:19:36 +0900

    :dizzy: code : Solve boj 15666 N과 M (12)

---
+++ BOJ/backtracking/15666-N과 M (12).py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+temp = []
+
+
+def dfs(start):
+    if len(temp) == m:
+        print(' '.join(map(str, temp)))
+        return
+
+    prev = 0
+    for i in range(start, n):
+        if prev != nums[i]:
+            temp.append(nums[i])
+            prev = nums[i]
+            dfs(i)
+            temp.pop()
+
+
+dfs(0)
