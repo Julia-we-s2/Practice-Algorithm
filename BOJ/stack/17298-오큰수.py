4e73ed6a6af857a9c23844f30f79d9d8236ccf16
위연주 <thizzyzz@naver.com>
2022-06-25 23:18:01 +0900

    :dizzy: code : Solve boj 17298 오큰수

---
+++ BOJ/stack/17298-오큰수.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+li = list(map(int, input().split()))
+ans = [-1] * n
+stack = []
+
+stack.append(0)
+for i in range(1, n):
+    while stack and li[stack[-1]] < li[i]:
+        ans[stack.pop()] = li[i]
+    stack.append(i)
+
+print(*ans)
