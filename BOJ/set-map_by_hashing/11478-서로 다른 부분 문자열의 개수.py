78da09adf45997daed0baadb7a21dcf3eeae44e1
위연주 <thizzyzz@naver.com>
2022-06-26 20:27:44 +0900

    :dizzy: code : Solve boj 11478 서로 다른 부분 문자열의 개수

---
+++ BOJ/set-map_by_hashing/11478-서로 다른 부분 문자열의 개수.py
@@ -0,0 +1,9 @@
+s = list(map(str, input().rstrip('\n')))
+temp = set()
+
+for i in range(len(s)):
+    for j in range(len(s) + 1):
+        if s[i:j]:
+            temp.add(''.join(s[i:j]))
+
+print(len(temp))
