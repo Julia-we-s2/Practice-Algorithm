fb8711ee23e7c3ce628bfc9896151ea18a1952cc
위연주 <thizzyzz@naver.com>
2022-07-08 23:40:56 +0900

    :dizzy: code : Solve boj 07662 이중 우선순위 큐

---
+++ BOJ/priority_queue/07662-이중 우선순위 큐.py
@@ -0,0 +1,51 @@
+# 내가 했다.. 멋져..
+# 각각의 테스트 케이스 안에 min_h, max_h, dic 초기화를 해야지 (조-심)
+
+import sys
+import heapq
+from collections import defaultdict
+input = sys.stdin.readline
+
+t = int(input())
+
+for tc in range(t):
+    min_h = []
+    max_h = []
+    dic = defaultdict(int)
+
+    k = int(input())
+
+    for _ in range(k):
+        operation, num = map(str, input().split())
+
+        if operation == 'I':
+            heapq.heappush(min_h, int(num))
+            heapq.heappush(max_h, -int(num))
+            dic[int(num)] += 1
+
+        else:
+            if num == '-1':
+                while min_h:
+                    temp = heapq.heappop(min_h)
+
+                    if dic[temp] > 0:
+                        dic[temp] -= 1
+                        break
+
+            elif num == '1':
+                while max_h:
+                    temp = -heapq.heappop(max_h)
+
+                    if dic[temp] > 0:
+                        dic[temp] -= 1
+                        break
+
+    li = []
+    for i in dic:
+        if dic[i] > 0:
+            li.append(i)
+
+    if li:
+        print(max(li), min(li))
+    else:
+        print('EMPTY')
