ff9e243c3fe21a13399e26d864f313d333301ac9
위연주 <thizzyzz@naver.com>
2022-07-12 10:40:16 +0900

    :heavy_plus_sign: add : 투 포인터를 이용한 풀이 추가 / boj 11728 배열 합치기

--- BOJ/sorting/11728-배열 합치기.py
+++ BOJ/sorting/11728-배열 합치기.py
@@ -1,5 +1,35 @@
+# 풀이 1: 배열을 합쳐서 정렬하기
+
 n, m = map(int, input().split())
 a_li = list(map(int, input().split()))
 b_li = list(map(int, input().split()))
 
 print(*sorted(a_li + b_li))
+
+
+
+# 풀이 2: 투 포인터를 이용하여 merge sort 하기 
+
+n, m = map(int, input().split())
+a_li = list(map(int, input().split()))
+b_li = list(map(int, input().split()))
+ans = []
+a, b = 0, 0
+
+while a != len(a_li) or b != len(b_li):
+    if a == len(a_li):
+        ans.append(b_li[b])
+        b += 1
+    elif b == len(b_li):
+        ans.append(a_li[a])
+        a += 1
+
+    else:
+        if a_li[a] < b_li[b]:
+            ans.append(a_li[a])
+            a += 1
+        else:
+            ans.append(b_li[b])
+            b += 1
+
+print(*ans)
