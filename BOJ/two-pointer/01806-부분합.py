7706375f89548f456b00e33746f93658e448354e
위연주 <thizzyzz@naver.com>
2022-07-19 23:03:59 +0900

    :dizzy: code : Solve boj 01806 부분합

---
+++ BOJ/two-pointer/01806-부분합.py
@@ -0,0 +1,20 @@
+n, s = map(int, input().split())
+nums = list(map(int, input().split()))
+
+start, end = 0, 0
+total = nums[0]
+ans = 1000000
+
+while True:
+    if total < s:
+        end += 1
+        if end == n:
+            break
+        total += nums[end]
+
+    elif total >= s:
+        total -= nums[start]
+        ans = min(ans, end - start + 1)
+        start += 1
+
+print(0 if ans == 1000000 else ans)
